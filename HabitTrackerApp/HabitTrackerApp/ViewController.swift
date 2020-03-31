636d92663783b2925ad2d699075f83dae88613be
Bernard <yosep0605@gmail.com>
2020-03-31 19:50:29 +0700

    + Initial Project Commit

---
+++ HabitTrackerApp/HabitTrackerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HabitTrackerApp
+//
+//  Created by Bernardinus on 31/03/20.
+//  Copyright © 2020 Bernardinus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
