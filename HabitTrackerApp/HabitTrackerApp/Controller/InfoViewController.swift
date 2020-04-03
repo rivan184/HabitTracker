7e363deccd1ce9a600baf2bad6a6b258f597aee5
Maria Jeffina <mariajeffina@gmail.com>
2020-04-03 21:43:16 +0700

    redo commit (imageView and segue to infoPage)

---
+++ HabitTrackerApp/HabitTrackerApp/Controller/InfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.swift
+//  HabitTrackerApp
+//
+//  Created by Maria Jeffina on 03/04/20.
+//  Copyright © 2020 Bernardinus. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func exitView(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- HabitTrackerApp/HabitTrackerApp/HomeViewController.swift
+++ HabitTrackerApp/HabitTrackerApp/HomeViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var infoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageViewSetting()
+    }
+    
+    //MARK: Image Function
+    func imageViewSetting(){
+        infoImage.layer.cornerRadius = 10
+        
+        //Tap Image interaction
+        infoImage.isUserInteractionEnabled = true
+        infoImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imageTap)))
+        
+    }
+    
+    @objc func imageTap(){
+        //To Present Modally
+        self.performSegue(withIdentifier: "viewInfo", sender: infoImage)
     }
 
 
