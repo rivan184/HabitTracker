ebaab82a94616eedaf527e525aa58ef606be636b
Maria Jeffina <mariajeffina@gmail.com>
2020-04-03 22:07:16 +0700

    redo commit (fix some errors, hopefully)

--- HabitTrackerApp/HabitTrackerApp/DummyData.swift
+++ HabitTrackerApp/HabitTrackerApp/DummyData.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+
+class DummyData: NSObject {
+    var name = ""
+    var goal = 0
+    var color:UIColor = .white
+    var tapProgress = 0
+        
+    init (name: String, goal: Int, color: UIColor, tapProgress: Int) {
+        self.name = name
+        self.goal = goal
+        self.color = color
+        self.tapProgress = tapProgress
+    }
+}
+
+struct HabitData {
+    //Data is a collection of habit based on model
+    var dataArray = [DummyData]()
+}
--- HabitTrackerApp/HabitTrackerApp/HabitCell/HabitTableViewCell.swift
+++ HabitTrackerApp/HabitTrackerApp/HabitCell/HabitTableViewCell.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class HabitTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var habitTitle: UILabel!
+    @IBOutlet weak var habitGoal: UILabel!
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    
+    var maxWidth = 358
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +25,26 @@ class HabitTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    // MARK: Model, Dummy Data
+    // Delete if not used
+    var model: DummyData? {
+        didSet{
+            cellConfig()
+        }
+    }
+    
+    func cellConfig(){
+        guard let habit = model else { return }
+        habitTitle.text = habit.name
+        habitGoal.text = String(habit.tapProgress)
+        
+        progressView.layer.cornerRadius = 10
+        progressView.layer.backgroundColor = habit.color.cgColor
+        widthConstraint.constant = CGFloat(Float(habit.tapProgress)/Float(habit.goal) * Float(self.maxWidth))
+        
+        progressView.updateConstraints()
     }
     
 }
