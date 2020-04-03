9bd4fad2deac7ef3fe656aeb77f6aca3845a2874
rivan184 <rayhanrivan@gmail.com>
2020-04-03 14:24:06 +0700

    + CustomTableViewCell

---
+++ HabitTrackerApp/HabitTrackerApp/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  HabitTrackerApp
+//
+//  Created by Rivan on 03/04/20.
+//  Copyright © 2020 Bernardinus. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var habitNameLabel: UILabel!
+    @IBOutlet weak var habitDescLabel: UILabel!
+    @IBOutlet weak var habitGoalLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
