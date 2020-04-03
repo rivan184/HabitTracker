505a064b8c13475888cff2be8f94b0c62427f0e6
rivan184 <rayhanrivan@gmail.com>
2020-04-03 15:06:11 +0700

    Custom Cell Name, Desc, Goal, and Color with dummy data

--- HabitTrackerApp/HabitTrackerApp/NewHabitViewController.swift
+++ HabitTrackerApp/HabitTrackerApp/NewHabitViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class NewHabitViewController: UIViewController {
+class NewHabitViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
+    struct Habit{
+        var habitName: String
+        var habitDesc: String
+        var habitGoal: Int
+        var habitColor: UIColor
+    }
+    
+    let habits = [
+        Habit(habitName: "Wash Hands", habitDesc: "Washing hand desc", habitGoal: 10, habitColor: UIColor(red: 88/255, green: 188/255, blue: 230/255, alpha: 1)),
+        Habit(habitName: "Take Vitamins",habitDesc: "Take Vitamins desc", habitGoal: 3, habitColor: UIColor(red: 229/255, green: 148/255, blue: 56/255, alpha: 1)),
+        Habit(habitName: "Do Exercise", habitDesc: "Do Exercise desc", habitGoal: 1, habitColor: UIColor(red: 221/255, green: 108/255, blue: 211/255, alpha: 1)),
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
     
     
-    @IBOutlet weak var addHabitButton: UIButton!
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return habits.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        let habit = habits[indexPath.row]
+        
+        cell.cellView.layer.cornerRadius = 20
+        
+        cell.habitNameLabel.text = habit.habitName
+        cell.habitDescLabel.text = habit.habitDesc
+        cell.habitGoalLabel.text = "\(habit.habitGoal)"
+        cell.cellView.backgroundColor = habit.habitColor
+        
+        return cell
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.addHabitBtn.layer.cornerRadius = 20
         
-    self.addHabitButton.layer.cornerRadius = 20
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func addHabitAction(_ sender: UIButton) {
+
+    @IBOutlet weak var addHabitBtn: UIButton!
+    @IBAction func addNewHabit(_ sender: Any) {
+        self.performSegue(withIdentifier: "toAddHabit", sender: nil)
     }
-    
+
 }
