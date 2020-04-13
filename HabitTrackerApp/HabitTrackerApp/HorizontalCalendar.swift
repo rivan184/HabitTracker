2269bc4159a0a5393728d2534bdac46b88373d12
Bernard <yosep0605@gmail.com>
2020-04-13 09:18:54 +0700

    + cleanup + add data

---
+++ HabitTrackerApp/HabitTrackerApp/HorizontalCalendar.swift
@@ -0,0 +1,21 @@
+//
+//  HorizontalCalendar.swift
+//  HabitTrackerApp
+//
+//  Created by Bernardinus on 12/04/20.
+//  Copyright © 2020 Bernardinus. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalCalendar: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
